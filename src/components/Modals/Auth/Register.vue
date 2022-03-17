14b10421045dd5bb3ec31f9fc7728996e74be867
Gabriel Brandão <gabrielbr.eng@gmail.com>
2022-03-16 22:22:04 -0300

    Registration Updates

--- src/components/Modals/Auth/Register.vue
+++ src/components/Modals/Auth/Register.vue
@@ -1,41 +1,21 @@
 <template>
   <modal id="register" width="25%">
-    <form class="space-y-6" action="#" method="POST">
-        <div>
-          <label for="name" class="block text-sm font-medium text-gray-700"> Full Name </label>
-          <div class="mt-1">
-            <input id="name" name="name" type="name" autocomplete="name" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-          </div>
-        </div>
-        <div>
-          <label for="email" class="block text-sm font-medium text-gray-700"> Email address </label>
-          <div class="mt-1">
-            <input id="email" name="email" type="email" autocomplete="email" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-          </div>
-        </div>
-
-        <div>
-          <label for="password" class="block text-sm font-medium text-gray-700"> Password </label>
-          <div class="mt-1">
-            <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-          </div>
-        </div>
-
-        <div class="flex items-center justify-between">
-          <div class="flex items-center">
-            <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
-            <label for="remember-me" class="ml-2 block text-sm text-gray-900"> Remember me </label>
-          </div>
-
-          <div class="text-sm">
-            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your password? </a>
-          </div>
-        </div>
-
-        <div>
-          <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Sign in</button>
-          <p class="mt-2 w-full font-medium text-center text-gray-600 hover:text-indigo-500"> Já possui conta? <span @click="$store.dispatch('modals/close', 'register'), $store.dispatch('modals/open', 'login')" class="text-indigo-600">Entrar</span> </p>
-        </div>
-      </form>
+    <!--Nome Completo-->
+    <label for="name" class="block text-sm font-medium text-gray-700"> Nome Completo </label>
+    <input id="name" name="name" type="name" autocomplete="name" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+    <!--Endereço de Email-->
+    <label for="email" class="mt-4 block text-sm font-medium text-gray-700"> Endereço de Email </label>
+    <input id="email" name="email" type="email" autocomplete="email" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+    <!--Senha-->
+    <label for="password" class="mt-2 w-full block text-sm font-medium text-gray-700"> Senha </label>
+    <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+    <!--Lembrança da Senha-->
+    <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
+    <label for="remember-me" class="block text-sm text-gray-900"> Lembre-me </label>
+    <!--Esqueceu sua Senha?-->
+    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Esqueceu sua Senha? </a>
+    <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Registre-se</button>
+    <!--Ja possui Conta?-->      
+    <p class="mt-2 w-full font-medium text-center text-gray-600 hover:text-indigo-500"> Já possui conta? <span @click="$store.dispatch('modals/close', 'register'), $store.dispatch('modals/open', 'login')" class="text-indigo-600">Entrar</span> </p>
   </modal>
 </template>
