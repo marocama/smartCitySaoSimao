5ee5af58f049c180d55bec5395b94e91169e01e4
Gabriel Brandão <gabrielbr.eng@gmail.com>
2022-06-08 13:50:02 -0300

    Update Footer

--- src/views/Public/Transport/Index.vue
+++ src/views/Public/Transport/Index.vue
@@ -30,41 +30,26 @@
               </div>
             </nav>
           </div> 
-
           <transition enter-active-class="duration-150 ease-out" enter-from-class="opacity-0 scale-95" enter-to-class="opacity-100 scale-100" leave-active-class="duration-100 ease-in" leave-from-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-95">
             <PopoverPanel focus class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
               <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                <div class="px-5 pt-4 flex items-center justify-between">
-                  <div>
-                    <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="" />
-                  </div>
-                  <div class="-mr-2">
-                    <PopoverButton class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                      <span class="sr-only">Close main menu</span>
-                      <XIcon class="h-6 w-6" aria-hidden="true" />
-                    </PopoverButton>
-                  </div>
-                </div>
+                <div class="px-5 pt-4 flex items-center justify-between"></div>
                 <div class="px-2 pt-2 pb-3 space-y-1">
                   <a v-for="item in navigation" :key="item.name" :href="item.href" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">{{ item.name }}</a>
                 </div>
-                <a href="#" class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"> Log in </a> 
               </div>
             </PopoverPanel>
           </transition> 
         </Popover> 
-
         <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
           <div class="sm:text-center lg:text-left">
             <h1 class="text-2xl tracking-tight font-extrabold text-gray-900 sm:text-2xl md:text-2xl">
               <span class="block xl:inline">ACESSIBILIDADE EM SÃO SIMÃO</span>
-              {{ ' ' }}
-              <!-- <span class="block text-indigo-600 xl:inline">online business</span> -->
             </h1>
             <p class="mt-3 text-justify text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">O projeto Transporte Acessível tem como intuito ajudar pessoas deficientes a se locomoverem com mais facilidade no dia a dia, nossa tecnologia se compromete com o avanço das adaptações de transportes para as comunidades que não tem acesso aos locais específicos que abrangem seu cotidiano.</p>
             <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
               <div class="rounded-md shadow">
-                <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"> Agende agora seu Transporte Acessível</a>
+                <a class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"> Agende agora seu Transporte Acessível</a>
               </div>
             </div>
           </div>
