b62ba78162efdfd100648340ae512b90f249480c
Gabriel Brandão <gabrielbr.eng@gmail.com>
2022-06-13 10:24:35 -0300

    Creating escorts in trasnport

--- src/components/Modals/Transport.vue
+++ src/components/Modals/Transport.vue
@@ -30,8 +30,22 @@
      <div class="mt-1">
       <input id="time" name="time" type="time" autocomplete="time" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
     </div>
-
-
+    <!--Acompanhantes-->
+    <div class="space-y-8 divide-y divide-gray-200">
+      <fieldset class="mt-6">
+        <legend class="contents text-base font-medium text-gray-900">Deseja levar Acompanhantes?</legend>
+        <div class="mt-4 space-y-4">
+            <div class="flex items-center">
+              <input id="yes" name="yes" type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
+              <label for="yes" class="ml-3 block text-sm font-medium text-gray-700"> Sim </label>
+            </div>
+          <div class="flex items-center">
+            <input id="no" name="no" type="radio" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300" />
+            <label for="no" class="ml-3 block text-sm font-medium text-gray-700"> Não </label>
+          </div>
+        </div>
+      </fieldset>
+    </div>
 
     <!-- Esqueci minha senha -->
     <button type="button" class="mt-2 w-full text-right font-medium text-sm text-indigo-600 hover:text-indigo-500">Esqueceu sua senha?</button>
