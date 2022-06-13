0afea459153102da4fbf7e3b1c3f2431254f0a1b
Gabriel Brandão <gabrielbr.eng@gmail.com>
2022-06-13 10:09:00 -0300

    Creating date and time in transport

--- src/components/Modals/Transport.vue
+++ src/components/Modals/Transport.vue
@@ -1,7 +1,7 @@
 <template>
   <modal id="transport" width="25%">
     <!-- Nome Completo -->
-    <label for="name" class="block text-sm font-medium text-gray-700"> Nome Completo </label>
+    <label for="name" class="block text-sm font-medium text-gray-700">Nome Completo</label>
     <div class="mt-1">
       <input id="name" name="name" type="name" autocomplete="name" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
     </div>
@@ -16,10 +16,23 @@
       <input id="homeAddress" name="homeAddress" type="homeAddress" autocomplete="homeAddress" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
     </div>
     <!-- Endereço Destino -->
-    <label for="destinationAddress" class="block text-sm font-medium text-gray-700">Endereço de Destino </label>
+    <label for="destinationAddress" class="block text-sm font-medium text-gray-700">Endereço de Destino</label>
      <div class="mt-1">
       <input id="destinationAddress" name="destinationAddress" type="destinationAddress" autocomplete="destinationAddress" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
     </div>
+    <!--Data-->
+    <label for="date" class="block text-sm font-medium text-gray-700">Data</label>
+     <div class="mt-1">
+      <input id="date" name="date" type="date" autocomplete="date" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+    </div>
+    <!--Horário-->
+    <label for="time" class="block text-sm font-medium text-gray-700">Horário</label>
+     <div class="mt-1">
+      <input id="time" name="time" type="time" autocomplete="time" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+    </div>
+
+
+
     <!-- Esqueci minha senha -->
     <button type="button" class="mt-2 w-full text-right font-medium text-sm text-indigo-600 hover:text-indigo-500">Esqueceu sua senha?</button>
     <!-- Entrar -->
