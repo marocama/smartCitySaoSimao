4b850b04dcda3b8629c2310419258ae3c36ddff9
Gabriel Brandão <gabrielbr.eng@gmail.com>
2022-07-26 14:42:12 -0300

    Updates Home

--- src/views/Public/Home.vue
+++ src/views/Public/Home.vue
@@ -103,30 +103,6 @@
         </div>
       </div>
     </div>
-
-    <!-- Logo Cloud -->
-    <div class="bg-gray-100">
-      <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
-        <p class="text-center text-sm font-semibold uppercase text-gray-500 tracking-wide">Trusted by over 5 very average small businesses</p>
-        <div class="mt-6 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
-          <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <img class="h-12" src="https://tailwindui.com/img/logos/tuple-logo-gray-400.svg" alt="Tuple">
-          </div>
-          <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <img class="h-12" src="https://tailwindui.com/img/logos/mirage-logo-gray-400.svg" alt="Mirage">
-          </div>
-          <div class="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-            <img class="h-12" src="https://tailwindui.com/img/logos/statickit-logo-gray-400.svg" alt="StaticKit">
-          </div>
-          <div class="col-span-1 flex justify-center md:col-span-2 md:col-start-2 lg:col-span-1">
-            <img class="h-12" src="https://tailwindui.com/img/logos/transistor-logo-gray-400.svg" alt="Transistor">
-          </div>
-          <div class="col-span-2 flex justify-center md:col-span-2 md:col-start-4 lg:col-span-1">
-            <img class="h-12" src="https://tailwindui.com/img/logos/workcation-logo-gray-400.svg" alt="Workcation">
-          </div>
-        </div>
-      </div>
-    </div>
     <!-- Gradient Feature Section -->
     <div class="bg-gradient-to-r from-purple-800 to-indigo-700">
       <div class="max-w-4xl mx-auto px-4 py-16 sm:px-6 sm:pt-20 sm:pb-24 lg:max-w-7xl lg:pt-24 lg:px-8">
@@ -307,13 +283,9 @@
               <p class="text-base leading-7">Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt vel ultricies. Vulputate aliquet velit faucibus semper. Pellentesque in venenatis vestibulum consectetur nibh id. In id ut tempus egestas. Enim sit aliquam nec, a. Morbi enim fermentum lacus in. Viverra.</p>
             </div>
           </div>
-
           <!-- Stats section -->
           <div class="mt-10">
-            <dl class="grid grid-cols-2 gap-x-4 gap-y-8">
-              
-            </dl>
-            
+            <dl class="grid grid-cols-2 gap-x-4 gap-y-8"></dl>
           </div>
         </div>
       </div>
