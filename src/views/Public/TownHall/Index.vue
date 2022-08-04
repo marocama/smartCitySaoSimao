b1f85d70fea73090c40b80ce34f5f79e4ad427d2
Gabriel Brandão <gabrielbr.eng@gmail.com>
2022-08-04 14:31:29 -0300

    Main Page Update TownHall

--- src/views/Public/TownHall/Index.vue
+++ src/views/Public/TownHall/Index.vue
@@ -1,13 +1,84 @@
 <template>
-  <main class="mt-2 mx-20">  
-    <nav class="justify-around -mb-px flex space-x-8" aria-label="Tabs">
-      <a v-for="(item, index) in options" :key="index" :href="item.path" class="py-4 px-1 inline-flex items-center text-gray-500 hover:text-gray-700 font-medium text-sm border-b-2 border-transparent hover:border-gray-300 group">
-        <svg class="text-gray-400 group-hover:text-gray-500 -ml-0.5 mr-2 h-5 w-5" preserveAspectRatio="xMidYMid meet" viewBox="0 0 20 20" v-html="item.icon" />
-        <span>{{ item.label }}</span>
-      </a>
-    </nav>
-     <button @click="menu = true" type="button" class="p-2 fixed right-7 w-min rounded-full shadow-sm bg-purple-600 text-white focus:outline-none" :class="$store.state.mobile ? 'bottom-20' : 'bottom-4'">
-      <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" /></svg>
+  <main class="mt-2 mx-20">
+    <div class="bg-white overflow-hidden">
+      <div class="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+        <div class="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen"></div>
+        <div class="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none">
+          <div>
+            <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">São Simão</h3>
+            <h1 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">O município</h1>
+          </div>
+        </div>
+        <div class="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
+          <div class="relative lg:row-start-1 lg:col-start-2">
+            <svg class="hidden lg:block absolute top-0 right-0 -mt-20 -mr-20" width="404" height="384" fill="none" viewBox="0 0 404 384" aria-hidden="true">
+              <defs>
+                <pattern id="de316486-4a29-4312-bdfc-fbce2132a2c1" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                  <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                </pattern>
+              </defs>
+              <rect width="404" height="384" fill="url(#de316486-4a29-4312-bdfc-fbce2132a2c1)" />
+            </svg>
+            <div class="relative text-base mx-auto max-w-prose lg:max-w-none">
+              <figure>
+                <div class="aspect-w-12 aspect-h-7 lg:aspect-none">
+                  <img class="rounded-lg shadow-lg object-cover object-center" src="https://static.wixstatic.com/media/cf9a27_1e03f8e62db04ed4b3f94bdf1d39dd26~mv2.jpg/v1/fill/w_838,h_560,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Morro%20do%20Cruzeiro.jpg" alt="Whitney leaning against a railing on a downtown street" width="1184" height="1376">
+                </div>
+              </figure>
+            </div>
+          </div>
+          <div class="mt-8 lg:mt-0">
+            <div class="text-justify max-w-prose mx-auto lg:max-w-none">
+              <p class="text-lg text-gray-500">O vale da Saúde. Possui uma identidade única no cenário regional, estadual e nacional e esta imagem é comprovada por fatos e documentos históricos como o “Berço da Proclamação da República” e “Terra de Marcelo Grassmann”.</p>
+            </div>
+            <div class="mt-5 prose prose-indigo text-justify text-gray-500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">  
+              <div class="lg:mt-5">
+                <p class="text-lg text-gray-500">Com uma área de 617.252 Km² e uma população de 15.446 habitantes (estimativa do IBGE 2021), São Simão esta localizada à nordeste do Estado de São Paulo. Há 282 Km de distância da capital, encravada entre morros possui vastas e belas paisagens naturais assim como uma longa e bela história que se confunde com a do estado e a da própria nação.</p>
+              </div>
+              <div class="lg:mt-5">
+                <p class="text-lg text-gray-500">Se destaca pela qualidade de vida, pela cultura e paisagens naturais, na indústria a produção de agua, argila branca, areia, jeans, poliestireno e moveis de escritório. Na agricultura a cana de açúcar e o reflorestamento para celulose e agricultura de subsistência.</p>
+              </div>
+            </div>
+            <div class="mt-8 inline-flex rounded-md shadow">
+              <a href="https://www.saosimao.sp.gov.br/inicial" class="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"> Acessar </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="relative py-16 bg-white overflow-hidden">
+        <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+        <div class="relative h-full text-lg max-w-prose mx-auto" aria-hidden="true">
+          <svg class="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+            <defs>
+              <pattern id="f210dbf6-a58d-4871-961e-36d5016a0f49" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+              </pattern>
+            </defs>
+            <rect width="404" height="384" fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
+          </svg>
+          <svg class="absolute bottom-12 left-full transform translate-x-32" width="404" height="384" fill="none" viewBox="0 0 404 384">
+            <defs>
+              <pattern id="d3eb07ae-5182-43e6-857d-35c643af9034" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+              </pattern>
+            </defs>
+            <rect width="404" height="384" fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)" />
+          </svg>
+        </div>
+      </div>
+      <div class="relative px-4 sm:px-6 lg:px-8">
+        <div class="text-lg max-w-prose mx-auto">
+          <h1>
+            <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Turismo</span>
+            <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">O Vale da Saúde</span>
+          </h1>
+          <p class="mt-8 text-xl text-center text-gray-500 leading-8">Clima privilegiado em virtude das serras que a cercam. Água potável e cristalina é encontrada em abundância, o que a faz ter potencial para estância hidromineral, climática e/ou turística.</p>
+        </div>
+      </div>
+    </div>
+    <button @click="menu = true" type="button" class="p-2 fixed right-7 w-min rounded-full shadow-sm bg-purple-600 text-white focus:outline-none" :class="$store.state.mobile ? 'bottom-20' : 'bottom-4'">
+    <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" /></svg>
     </button>
     <SliderOver v-model="menu" :size="$store.state.mobile ? 'sm' : 'xs'" title="Categoria" subtitle="Filtre os resultados da sua busca.">
       <ul class="grid grid-cols-2 gap-3">
@@ -22,7 +93,9 @@
 
 <script>
 export default {
+  
   components: {
+
     SliderOver: () => import('@/components/SlideOver/Default'),
 },
   data() { return {
