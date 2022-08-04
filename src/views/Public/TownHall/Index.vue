fd919e2c8f3d33428378b12a8378856c00e27016
Gabriel Brandão <gabrielbr.eng@gmail.com>
2022-08-04 10:18:01 -0300

    Upload Modelo Logo

--- src/views/Public/TownHall/Index.vue
+++ src/views/Public/TownHall/Index.vue
@@ -1,21 +1,11 @@
 <template>
-  <main class="mt-2 mx-20">
-    <div class="mb-5">
-      <div class="sm:hidden">
-        <label for="tabs" class="sr-only">Select a tab</label>
-      </div>
-      <div class="hidden sm:block">
-        <div class="border-b border-gray-200">
-          <nav class="justify-around -mb-px flex space-x-8" aria-label="Tabs">
-            <a v-for="(item, index) in options" :key="index" :href="item.path" class="py-4 px-1 inline-flex items-center text-gray-500 hover:text-gray-700 font-medium text-sm border-b-2 border-transparent hover:border-gray-300 group">
-              <svg class="text-gray-400 group-hover:text-gray-500 -ml-0.5 mr-2 h-5 w-5" preserveAspectRatio="xMidYMid meet" viewBox="0 0 20 20" v-html="item.icon" />
-              <span>{{ item.label }}</span>
-            </a>
-          </nav>
-        </div>
-      </div>
-    </div>
-    <router-view />
+  <main class="mt-2 mx-20">  
+    <nav class="justify-around -mb-px flex space-x-8" aria-label="Tabs">
+      <a v-for="(item, index) in options" :key="index" :href="item.path" class="py-4 px-1 inline-flex items-center text-gray-500 hover:text-gray-700 font-medium text-sm border-b-2 border-transparent hover:border-gray-300 group">
+        <svg class="text-gray-400 group-hover:text-gray-500 -ml-0.5 mr-2 h-5 w-5" preserveAspectRatio="xMidYMid meet" viewBox="0 0 20 20" v-html="item.icon" />
+        <span>{{ item.label }}</span>
+      </a>
+    </nav>
      <button @click="menu = true" type="button" class="p-2 fixed right-7 w-min rounded-full shadow-sm bg-purple-600 text-white focus:outline-none" :class="$store.state.mobile ? 'bottom-20' : 'bottom-4'">
       <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" /></svg>
     </button>
@@ -36,6 +26,9 @@ export default {
     SliderOver: () => import('@/components/SlideOver/Default'),
 },
   data() { return {
+
+    itens: [],
+    selected: '',
     menu: false,
     options: [
       { label: 'Página Inicial', path: 'https://www.saosimao.sp.gov.br/', icon: `<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" fill="currentColor" />` },
