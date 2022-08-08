6a015805fcf904f4ab464bf4a4576293b0b18fad
Gabriel Brandão <gabrielbr.eng@gmail.com>
2022-08-08 20:01:08 -0300

    Updates

--- src/views/Public/SmartSolutions/OrganicAds/Index.vue
+++ src/views/Public/SmartSolutions/OrganicAds/Index.vue
@@ -14,7 +14,7 @@
                     <svg class="hidden lg:block absolute top-0 right-0 -mt-20 -mr-20" width="404" height="384" fill="none" viewBox="0 0 404 384" aria-hidden="true">
                     <defs>
                         <pattern id="de316486-4a29-4312-bdfc-fbce2132a2c1" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                        <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+                        <rect x="0" y="0" width="4" height="4" class="text-gray-300" fill="currentColor" />
                         </pattern>
                     </defs>
                     <rect width="404" height="384" fill="url(#de316486-4a29-4312-bdfc-fbce2132a2c1)" />
@@ -40,7 +40,7 @@
                     </div>
                     </div>
                     <div class="mt-8 inline-flex rounded-md shadow">
-                    <a href="#" class="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"> Anuncie seu Produto </a>
+                    <button href="#" class="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"> Anuncie seu Produto </button>
                     </div>
                 </div>
                 </div>
